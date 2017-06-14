3fdeb8a84c9450ae07e3f6569df7a81c9a44a3e9
Syam Vangara <syamsundar.v.g@gmail.com>
2017-06-14 16:21:18 -0400

    sync June 14 4:21 pm

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 name=$(sed -n "6 p" poisson.in)
 #echo -n "Write data to folder > "
 #read name
-./poisson -ksp_type gmres -log_view #-ksp_monitor_max #-mat_view ::ascii_matlab
+#./poisson -ksp_type gmres -log_view #-ksp_monitor_max #-mat_view ::ascii_matlab
+#./poisson -pc_type jacobi -ksp_type richardson -ksp_richardson_scale 0.66666666 -log_view -ksp_monitor_max #-mat_view ::ascii_matlab
+./poisson -pc_type jacobi -ksp_type gmres -log_view -ksp_monitor_max #-mat_view ::ascii_matlab
 mkdir -p data/$name
 #mv -v *.out data/$name/
 cp -r *.in data/$name/
